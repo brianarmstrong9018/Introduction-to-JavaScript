46bfbba97cd94a7546ef9da9c89143f7dd542bda
brianarmstrong9018 <brianarmstrong9018@gmail.com>
2021-05-11 21:11:01 -0500

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ Do the following:
    2. Console log true if age is 18 or higher
    HINT: no function required
 */
-let votingAge = 18;
-if(votingAge > 18){
-  console.log('true')
-} else{
-  console.log('false')
+
+let votingAge = "20";
+
+if (votingAge >= 18) {
+  console.log(true);
 }
 
 
@@ -31,14 +31,16 @@ Do the following:
    3. Console log the new value of the 1st variable
    HINT: no function required
 */
-var num1 = 15;
-var num2 = 16;
-if(num1 === 15){
-  console.log(num1 = num2);
+
+let myName = "brian";
+
+let isArealBoy = "yes";
+
+if (isArealBoy === "yes") {
+  myName = "pinoccio"
 }
 
-
-
+console.log(myName);
 
 /*
 Task 1c - Convert Strings to Numbers
@@ -49,7 +51,9 @@ Do the following:
    HINT: look up the Number method
 */
 
+const party ="1999";
 
+console.log(Number(party));
 
 
 /*
@@ -57,30 +61,30 @@ Task 1d - Multiply
  
 Do the following:   
    1. Invoke the multiply function below and pass it two numbers
-   2. Receive the numbers in the parameters: a and b
+   2. Receive the parameters: a and b
    3. Multiply a and b and return the answer
 */
 
-function multiply(a,b){
-    return a * b   
-  }
+function multiply(a, b){
+  return a * b;
+}
 
-
+console.log(multiply(7,9))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
 //Age in Dog years
 /*
 Do the following:
-   1. Invoke the dogYears function below and pass an age value to it
-   2. Use the received value to calculate the age in dog years (1 human year is equal to 7 dog years)
-   3. Return the newly calculated age
+ 1. Invoke the dogYears function below and pass an age value to it
+ 2. Use the received value to calculate the age in dog years (1 human year is equal to 7 dog years)
+ 3. Return the newly calculated age
 */
 
-function dogYears(years){
-    return years * 7;
+function dogYears(age){
+  return age * 7;
 }
-
+ console.log(dogYears(3))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -88,51 +92,47 @@ function dogYears(years){
 //Dog feeder - Depending on their weight and age, we need to know how many pounds of food to feed our dog each day!
 /*
 Use the hungryDog function and feeding requirements below to do the following:
-  1. Invoke the hungryDog function below and pass it a weight value in pounds
-  2. Also, pass to hungryDog an age value in years (note: if the dog is a puppy, the age will be a decimal. For example: three months = 3/12 or .25)
-  3. Do the proper calculations and return the number of pounds of raw food to feed the dog/puppy in a day
+1. Invoke the hungryDog function below and pass it a weight value in pounds
+2. Also, pass to hungryDog an age value in years (note: if the dog is a puppy, the age will be a decimal. For example: three months = 3/12 or .25)
+3. Do the proper calculations and return the number of pounds of raw food to feed the dog/puppy in a day
+
+REMEMBER: This program should handle correctly adult AND puppy ages and weights
+
+Feeding Requirements:
+Adult Dogs 1 year and older 
+   up to 5 lbs - 5% of their body weight
+   6 - 10 lbs - 4% of their body weight 
+   11 - 15 lbs - 3% of their body weight 
+   > 15lbs - 2% of their body weight 
+Puppies less than 1 year
+   2 - 4 months 10% of their body weight
+   4 - 7 months 5% of their body weight 
+   7 - 12 months 4% of their body weight
   
-  REMEMBER: This program should handle correctly adult AND puppy ages and weights
-  
-  Feeding Requirements:
-  Adult Dogs 1 year and older 
-     up to 5 lbs - 5% of their body weight
-     6 - 10 lbs - 4% of their body weight 
-     11 - 15 lbs - 3% of their body weight 
-     > 15lbs - 2% of their body weight 
-  Puppies less than 1 year
-     2 - 4 months 10% of their body weight
-     4 - 7 months 5% of their body weight 
-     7 - 12 months 4% of their body weight
-    
-  NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
+NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(age, weight){
-    if(age > 1 && weight < 5){ 
-        return 0.44999999999999996 * weight; 
-      }
-      else if(age > 1 && weight >= 6 || weight <= 10){
-        return 0.04 * weight;
-      }
-      else if(age > 1 && weight >= 11 || weight <= 15 ){
-        return 0.03 * weight;
-      }
-      else if(age > 1 && weight > 15){
-        return 0.02 * weight;
-      }
-      else if(age < 1 && weight >= 0.166 || weight <= 0.333){
-        return 0.10 * weight;
-      }
-      else if(age < 1 && weight >= 0.333 || weight <= 0.583){
-        return 0.05 * weight;
-      }
-      else if(age < 1 && weight >= 0.583 || weight <= 1.0){
-        return 0.04 * weight;
-      }
-    }
+function hungryDog(weight, age){
+  if (age >= 1 && weight <= 5) {
+    return weight * 0.05;
+  } else if (age >= 1 && weight >= 6 && weight <= 10) {
+    return weight * 0.04;
+  } else if (age >= 1 && weight >= 11 && weight <= 15) {
+    return weight * 0.03;
+  } else if (age >= 1 && weight > 15) {
+    return weight * 0.02; 
+  } else if (age < 1 && age >= 0.583) {
+    return weight * 0.04;
+  } else if (age < 0.583 && age >= .333) {
+    return weight * 0.05;
+  } else if (age < 0.333) {
+    return weight * 0.10;
+  } else{
+    return 'meow';
+  }   
+}
 
-  console.log(hungryDog(0.5, 12))
+console.log(hungryDog(15, 1));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -142,39 +142,42 @@ Create a global variable that randomly generates the computer's choice
 Use Math.random to determine the computers choice (Math.random gives a random number between 0 and 1)
 HINT: While you can complete this with only conditionals based on strings, it may help to equate choice to a number when using Math.random()
 Use the game function below to do the following:
-  1. Receive 2 parameters the user's choice and the computer's choice
-  2. Return whether the user won, lost, or tied based on these rules of the game described below - the strings returned need to match the strings below exactly.
-   - win should return "you win!"
-   - lose should return "you lose!"
-   - tie should return "it's a tie"
-  
-  RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
-  
-  HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
+1. Receive 2 parameters the user's choice and the computer's choice
+2. Return whether the user won, lost, or tied based on these rules of the game described below - the strings returned need to match the strings below exactly.
+ - win should return "you win!"
+ - lose should return "you lose!"
+ - tie should return "it's a tie"
+
+RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
+
+HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
-let computer = Math.random();  
-  if (computer < 0.34) {
-    computer = "rock";
-} else if(computer <= 0.67 && computer > 0.34) {
-    computer = "paper";
-} else {
-    computer = "scissors";
+
+let computer = Math.random();
+
+if (computer <= 0.34){
+  computer = 'rock';
+} else if (computer <= 0.67){
+  computer = 'paper';
+} else if (computer > 0.67){
+  computer = 'scissors';
 }
 
 function game(user, computer){
-  
-  if(user === 'paper' && computer === 'rock' || user === 'rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper' ){
-    return "you win!"
-  }
-  else if(user === 'paper' && computer === 'scissors' || user === 'rock' && computer === 'paper' || user === 'scissors' && computer === 'rock'){
-    return "you lose!"
-  }
-  
-  else if(user === 'scissors' && computer === 'scissors' || user === 'paper' && computer === 'paper' || user === 'rock' && computer === 'rock'){
-    return "it's a tie"
-  }
+  if(user === computer){
+    return `it's a tie`;
+  } else if (user === 'rock' && computer === 'scissors'){
+    return `you win!`;
+  } else if (user === 'paper' && computer === 'rock'){
+    return `you win!`;
+  } else if (user === 'scissors' && computer === 'paper'){
+    return `you win!`;
+  }else{
+    return `you lose!`;
+  } 
 }
 
+console.log(game('paper', computer));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -182,115 +185,112 @@ function game(user, computer){
 //Task 5a - KM to Miles 
 /*
 Using the miles function below do the following:
-  1. Receive a number of kilometers
-  2. Convert the number of kiolmeters received to miles
-  3. Return the number of miles
+1. Receive a number of kilometers
+2. Convert the number of kiolmeters received to miles
+3. Return the number of miles
 */
 
-function miles(km){
-  return 0.621371 * km;
-  }
+function miles(kilometers){
+  return kilometers * 0.621371;
+}
 
-
+console.log(miles(200));
 
 //Task 5b - Feet to CM
 /*
 Using the feet function below do the following:
-  1. Receive a number of cm
-  2. Convert the number of cm to feet
-  3. Return number of feet
+1. Receive a number of cm
+2. Convert the number of cm to feet
+3. Return number of feet
 */
 
 function feet(cm){
-    return cm / 30.48;
-  }
-//  console.log(feet(3))
+  return cm / 30.48;
+}
 
+console.log(feet(100));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
 // Let's Sing 99 Bottles of Soda on the Wall!
 /*
 Using the annoyingSong function below do the following:
-  1. Receive a starting number and start the count down from the number received 
-  2. At each invocation, it should RETURN this string (note: the tests are expecting the same string as below):
-      "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
+1. Receive a starting number and start the count down from the number received 
+2. At each invocation, it should RETURN this string (note: the tests are expecting the same string as below):
+    "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
 function annoyingSong(number){
-        for(let i = number; i > 0; i--){
-          return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-1} bottles of soda on the wall`
-        }
-  }
-    annoyingSong(5);
-    // console.log(annoyingSong(5))
+      for(let i = number; i > 0; i --){
+        return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i -1} bottles of soda on the wall`;
+      }
+}
+
+console.log(annoyingSong(10));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
 //Grade Calculator
 /*
 Using the grade function below do the following: 
-  1. Receive a score out of 100 
-  2. Return the corresponding letter grade following this grade scale:
-   90-100 should return 'you got an A' 
-   80-89 should return 'you got a B'
-   70-79 should return 'you got a C'
-   60-69 should return 'you got a D'
-   below should return 'you got an F'
+1. Receive a score out of 100 
+2. Return the corresponding letter grade following this grade scale:
+ 90-100 should return 'you got an A' 
+ 80-89 should return 'you got a B'
+ 70-79 should return 'you got a C'
+ 60-69 should return 'you got a D'
+ below should return 'you got an F'
 */
-  
+
 function grade(score){
-  if (score >= 90 && score <= 100){
-    return 'you got an A'
-  }
-  else if(score >= 80 && score <= 89){
-    return 'you got a B'
-  }
-  else if (score >= 70 && score <= 79){
-    return 'you got a C'
-  }
-  else if (score >= 60 && score <= 69){
-    return 'you got a D'
-  }
-  else if (score < 60){
-    return 'you got an F'
-  }
+if (score >= 90){
+  return 'you got an A';
+} else if (score < 90 && score >= 80){
+  return 'you got a B';
+} else if (score < 80 && score >= 70){
+  return 'you got a C';
+} else if (score < 70 && score >= 60){
+  return 'you got a D';
+} else if (score < 60);{
+  return 'you got an F';
 }
-  
-  
+}
+
+console.log(grade(100));
+
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
 //Vowel Counter - How many vowels are there?
 /*
 Using the vowelCounter function below do the following:
-  1. Receive a string as a parameter
-  2. Count and return the number of vowels within that string.  It should handle both capitalized and uncapitalized vowels.
-  HINT - you may need to study tomorrow's content on arrays 
-  HINT - try looking up the .includes() method
+1. Receive a string as a parameter
+2. Count and return the number of vowels within that string.  It should handle both capitalized and uncapitalized vowels.
+HINT - you may need to study tomorrow's content on arrays 
+HINT - try looking up the .includes() method
 */
 
 
-function vowelCounter() {
-    
+function vowelCounter(/*add your code here*/) {
+  /*add your code here*/
 }
 
 
 
 /*🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑*/
 function foo(){
-    //console.log('its working');
-    return 'bar';
+  //console.log('its working');
+  return 'bar';
 }
 /*🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Don't touch the code after this line! 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑*/
 export default{
-    foo,
-    multiply,
-    dogYears,
-    hungryDog,
-    game,
-    miles,
-    feet,
-    annoyingSong,
-    grade
+  foo,
+  multiply,
+  dogYears,
+  hungryDog,
+  game,
+  miles,
+  feet,
+  annoyingSong,
+  grade
 }
